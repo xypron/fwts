xypron/fwts

commit f36ff824d14531b9ab3c4a859f31a1ede384d5d2

lib: disable /dev/mem access on aarch64
There is no valid use-case for having the /dev/mem interface enabled,
let alone using it, on a multi-user aarch64 system. So turn fwts_mmap/
fwts_munmap into no-ops here.

Signed-off-by: Leif Lindholm <leif.lindholm@linaro.org>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Colin Ian King <colin.king@canonical.com>

src/lib/include/fwts.h
@@ -98,6 +98,7 @@
 #define FWTS_ARCH_INTEL	1
 #define FWTS_HAS_ACPI	1
 #define FWTS_HAS_UEFI	1
+#define FWTS_USE_DEVMEM 1
 #endif
 
 #if defined(__aarch64__)
@@ -108,11 +109,13 @@
 
 #if defined(__s390x__)
 #define FWTS_ARCH_S390X	1
+#define FWTS_USE_DEVMEM 1
 #endif
 
 #if defined(__PPC64__)
 #undef FWTS_HAS_ACPI
 #undef FWTS_HAS_UEFI
+#define FWTS_USE_DEVMEM 1
 #endif
 
 /* verision 3-tuple into integer */

src/lib/src/fwts_mmap.c
@@ -42,6 +42,7 @@ size_t fwts_page_size(void)
 	return (size_t)(page_size == -1 ? FWTS_DEFAULT_PAGE_SIZE : page_size);
 }
 
+#ifdef FWTS_USE_DEVMEM
 /*
  *  fwts_mmap()
  *	Try and map physical memory from offset address 'start' and length
@@ -88,3 +89,15 @@ int fwts_munmap(void *mem, const size_t size)
 
 	return FWTS_OK;
 }
+#else /* FWTS_USE_DEVMEM */
+void *fwts_mmap(const off_t start __attribute__ ((unused)),
+		const size_t size __attribute__ ((unused)))
+{
+	return FWTS_MAP_FAILED;
+}
+
+int fwts_munmap(void *mem __attribute__ ((unused)),
+		const size_t size __attribute__ ((unused)))                                                                        {
+	return FWTS_ERROR;
+}
+#endif /* FWTS_USE_DEVMEM */

commit aa52168a35e5034eb10e53e614e5519635c51975

lib: enable /dev/mem access on aarch64
The SMBIOS3 table supplied by U-Boot cannot be read without mmap.

Cc: Leif Lindholm <leif@nuviainc.com>
Fixes: f36ff824d145 ("lib: disable /dev/mem access on aarch64")
Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

src/lib/include/fwts.h
@@ -105,6 +105,7 @@
 #define FWTS_ARCH_AARCH64	1
 #define FWTS_HAS_ACPI	1
 #define FWTS_HAS_UEFI	1
+#define FWTS_USE_DEVMEM 1
 #endif
 
 #if defined(__s390x__)